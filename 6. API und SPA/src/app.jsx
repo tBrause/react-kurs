0bd934cdf43ff6cc3edaf837a310b47face2dd99
tBrause <brause.torsten@gmail.com>
2023-03-24 20:23:22 +0100

    edit

--- 6. API und SPA/src/app.jsx
+++ 6. API und SPA/src/app.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import LocationFinder from './components/LocationFinder';
+import MoviesFinder from './components/MoviesFinder';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
 		<LocationFinder />
+		{/* <MoviesFinder /> */}
 	</React.StrictMode>
 );
